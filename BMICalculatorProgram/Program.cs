3d2271dd4086919d16b66c63619f07b38c87ee53
hemanth111p <72683647+hemanth111p@users.noreply.github.com>
2021-06-24 21:02:55 +0530

    uploaded

---
+++ BMICalculatorProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculatorProgram
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            UserInputDatas data = new UserInputDatas();
+            Console.WriteLine("enter user height");
+            data.SetUserHeight(Console.ReadLine());
+            Console.WriteLine("enter user weight");
+            data.SetUserHeight(Console.ReadLine());
+            data.calculation();
+
+
+
+
+
+
+        }
+    }
+}
