9c1fc721c674462fe40df889a4ae2f89235aebec
hemanth111p <72683647+hemanth111p@users.noreply.github.com>
2021-06-26 11:34:50 +0530

    uploaded

---
+++ collegeIdCardDetails/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace college
+{
+    //Teacher  is-a Collegemember
+    class Teacher : CollegeMember
+    {
+
+
+        private string _dateOfJoining;
+        private string position;
+        public Teacher(string dateOfJoining, string _position, int idNumber, string name, int contactNumber, string department, string dateOfBirth, string address, string bloodGroup) : base(idNumber, name, contactNumber, department, dateOfBirth, address, bloodGroup)
+        {
+            this._dateOfJoining = dateOfJoining;
+            this.position = _position;
+        }
+
+    }
+
+}
